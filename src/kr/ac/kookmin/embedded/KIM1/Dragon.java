61da5a952befc925e49eb20d5af788db26d3dbef
KIMSEIHEE <sara940569@gmail.com>
2015-10-07 17:36:13 +0900

    Mobile-programming-5th-week

---
+++ src/kr/ac/kookmin/embedded/KIM1/Dragon.java
@@ -0,0 +1,8 @@
+package kr.ac.kookmin.embedded.KIM1;
+
+public class Dragon extends Monster{
+	boolean frighten(int degree){
+		System.out.println("breath fire");
+		return true;
+	}
+}
---
+++ src/kr/ac/kookmin/embedded/KIM1/Monster.java
@@ -0,0 +1,8 @@
+package kr.ac.kookmin.embedded.KIM1;
+
+public class Monster{
+	boolean frighten(int d){
+		System.out.println("arrrgh");
+		return true;
+	}
+}
